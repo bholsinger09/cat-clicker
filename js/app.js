0bac02019a039ece80681fd03968e4db603fe732
benjamin l holsinger <37024167+bholsinger09@users.noreply.github.com>
2018-10-28 01:44:44 -0600

    recent update
    added all 5 cats 
    all 5 cats hidden

--- js/app.js
+++ js/app.js
@@ -1,8 +1,37 @@
-var count = 0;
-$('#cat-img').click(function(e) {
-  let counterBox = $('#counter');
+function Cat1() {}
 
-  count += 1;
-  console.log(count);
-  counterBox.prepend(count); //the element has been clicked... do stuff here
-});
+var count;
+count = 0;
+Cat1.prototype.imageListener = function() {
+  $("#cat-img").click(function() {
+    let counterBox = $("#counter1");
+    count += 1;
+    console.log(count);
+    counterBox.prepend(count + " , ");
+  });
+};
+
+var kitten = new Cat1();
+
+kitten.imageListener();
+
+
+
+
+
+function Cat2() {}
+
+var count2;
+count2 = 0;
+Cat2.prototype.imageListener = function() {
+  $("#cat-img2").click(function() {
+    let counterBox = $("#counter2");
+    count2 += 1;
+    console.log(count2);
+    counterBox.prepend(count2 + " , ");
+  });
+};
+
+var kitten2 = new Cat2();
+
+kitten2.imageListener();
